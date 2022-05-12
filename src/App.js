2a798db55b1b5c3284125506c973a9d33088e025
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2022-05-12 18:16:15 +0530

    assesment done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import axios from "axios";
 import Carousel from "./Components/Carousel/Carousel";
 
-import { AiOutlineArrowLeft } from "react-icons/ai";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineRight } from "react-icons/ai";
 import { BiShuffle } from "react-icons/bi";
 
-
 const getApiForRandomPhoto = (count) => {
-	return `https://api.unsplash.com/photos/random/?count=${count}&client_id=QeKhrpQo6kuHZZUebvFxV1OD2bJMExLwyT2f6TflsBE`;
+	return `https://api.unsplash.com/photos/random/?count=${count}&client_id=haxmA_JmfVAFva4kzgkcnS-Vd8-bfXWKFVteJjJDJM8`;
+};
+
+const getApiForSearch = (photo) => {
+    return `https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=haxmA_JmfVAFva4kzgkcnS-Vd8-bfXWKFVteJjJDJM8`;
 };
 
 function randomIntFromInterval(min, max) {
@@ -17,15 +20,34 @@ function randomIntFromInterval(min, max) {
 }
 
 const getRandomPhoto = async () => {
-	const randomNumber = randomIntFromInterval(7, 15);
+	const randomNumber = randomIntFromInterval(1, 20);
 	const apiPath = getApiForRandomPhoto(randomNumber);
 	const { data } = await axios.get(apiPath);
 	return data;
 };
 
+const searchPhoto = async (photo) => {
+    const apiPath = getApiForSearch(photo);
+    const {data} = await axios.get(apiPath);
+    return data.results;
+}
+
 function App() {
 	const [childIndex, setChildIndex] = React.useState(0);
 	const [data, setData] = React.useState([]);
+	const [photo, setPhoto] = React.useState("");
+
+	const handleChange = (event) => {
+		setPhoto(event.target.value);
+	};
+
+	const handleSubmit = () => {
+        searchPhoto(photo.results).then((photos)=>{
+            console.log(photos);
+            setData(photos);
+        })
+	};
+
 	const randomizeImages = () => {
 		getRandomPhoto().then((randomPhotos) => {
 			setData(randomPhotos);
@@ -33,6 +55,7 @@ function App() {
 	};
 
 	React.useEffect(() => {
+        handleSubmit();
 		randomizeImages();
 	}, []);
 
@@ -61,18 +84,28 @@ function App() {
 		<div className='App'>
 			<div className='header'>
 				<h1>Gallery</h1>
+				<input
+					onChange={handleChange}
+					type='text'
+					name='photo'
+					placeholder='Search for Photos...'
+					className='search'
+				/>
+				<button onClick={handleSubmit} type='submit'>
+					Search
+				</button>
 				<button onClick={randomizeImages}>
 					Shuffle&nbsp;
-                    <BiShuffle />
+					<BiShuffle />
 				</button>
 			</div>
 			<Carousel data={data} />
 			<div className='cards-scroll'>
 				<button onClick={prevImage}>
-                    <AiOutlineArrowLeft />
+					<AiOutlineLeft />
 				</button>
 				<button onClick={nextImage}>
-                    <AiOutlineArrowRight />
+					<AiOutlineRight />
 				</button>
 			</div>
 		</div>
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Card.css";
+import { BiDownload } from "react-icons/bi";
+import { saveAs } from "file-saver";
+
 const HEIGHT = window.innerWidth < 720 ? 200 : 400;
 
 export const Card = (props) => {
 	const imageURL = props.urls.small;
 	const width = (props.width * HEIGHT) / props.height;
+
+	const downloadImage = async () => {
+		let blob = await fetch({ imageURL }).then((r) => r.blob());
+		var filesaver = saveAs(blob, "image.jpeg");
+	};
+
 	return (
 		<div className='card-container'>
 			<img src={imageURL} alt='' style={{ height: HEIGHT, width }} />
@@ -14,6 +23,11 @@ export const Card = (props) => {
 				<div className='photographer-details'>
 					<p>{`${props.user.first_name} ${props.user.last_name}`}</p>
 					<span>{`${props.likes} likes recieved`}</span>
+					<div className='download'>
+							<a href={imageURL} target='_blank' onClick={downloadImage}>
+								<BiDownload />
+							</a>
+					</div>
 				</div>
 			</div>
 		</div>
